b07cfb5e93757342045ac6f2ca336b9ec3f1629c
harish <hb.harishbalaji@gmail.com>
2016-09-02 13:37:43 -0400

    math2

---
+++ src/math2/math2.java
@@ -0,0 +1,14 @@
+package math2;
+
+public class math2 {
+	 public static void main(String[] args) {
+	 int mynumber;
+	 double myothernumber;
+
+	 mynumber = 2;
+	 myothernumber = 1.7938;
+	 
+	 System.out.println("My number is " + mynumber);
+	 System.out.println("My other number is " + myothernumber);
+	 }
+}
